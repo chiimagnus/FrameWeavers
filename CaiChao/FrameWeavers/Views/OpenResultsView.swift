a0d44f684ab0565753f3cc274ebf811386d222ce
𝓒𝓱𝓲𝓲 𝓜𝓪𝓰𝓷𝓾𝓼 <chii_magnus@outlook.com>
2025-07-25 01:22:04 +0800

    feat(OpenResultsView): 更新封面图片并调整布局
    - 将 Image("包装袋") 替换为 Image("封面")
    - 用 .scaledToFit() 替换 .frame(width: 100, height: 100) 以适应图片大小
    - 添加 .padding(.horizontal, 20) 以调整图片与文本的水平间距
    - 注释掉原有的 .frame(width: 100, height: 100) 代码行

--- CaiChao/FrameWeavers/Views/OpenResultsView.swift
+++ CaiChao/FrameWeavers/Views/OpenResultsView.swift
@@ -6,10 +6,12 @@ struct OpenResultsView: View {
     
     var body: some View {
         VStack(spacing: 40) {
-            Image("包装袋")
+            Image("封面")
                 .resizable()
-                .frame(width: 100, height: 100)
+                .scaledToFit()
+                // .frame(width: 100, height: 100)
                 .shadow(radius: 10)
+                .padding(.horizontal, 20)
 
             Text("在钢筋水泥的都市中，藏着一片不为人知的秘境。少女爱丽丝，一个能听懂风语花言的女孩，在一次午后小憩中，无意间听到了来自古老精灵的微弱呼唤。为了拯救被污染的自然，她必须与精灵签下契约，然而契约的代价，是她自己的生命力……")
                 .font(.custom("Kaiti SC", size: 16))
